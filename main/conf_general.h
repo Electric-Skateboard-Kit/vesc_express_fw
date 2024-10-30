3aa022cce8f1a0c00e685932344c2c4c64fd9238
CCarito <corralcarolina03@gmail.com>
2024-10-30 18:06:47 -0300

    Firmware Update for Version 3 Skateboard Board
    Since there is more than one hardware version, a firmware correction was made
    for the board currently being tested on the skateboard. This board is version
    number 3

--- main/conf_general.h
+++ main/conf_general.h
@@ -29,8 +29,8 @@
 #define FW_TEST_VERSION_NUMBER		0
 
 #if !defined(HW_SOURCE) && !defined(HW_HEADER)
-#define HW_HEADER					"hw_xp_t.h"
-#define HW_SOURCE					"hw_xp_t.c"
+#define HW_HEADER					"hw_lectec.h"
+#define HW_SOURCE					"hw_lectec.c"
 
 //#define HW_HEADER					"hw_str365.h"
 //#define HW_SOURCE					"hw_str365.c"
---
+++ main/hwconf/lectec/hw_lectec.c
@@ -0,0 +1,40 @@
+/*
+	Copyright 2022 Benjamin Vedder	benjamin@vedder.se
+
+	This file is part of the VESC firmware.
+
+	The VESC firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The VESC firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#include "hw_lectec.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+void hw_init(void) {
+
+		uart_driver_delete(UART_NUM_0);
+		//gpio_reset_pin(LED_RGB_PIN);
+
+	    gpio_config_t io_conf = {};
+	    io_conf.intr_type = GPIO_INTR_DISABLE;
+	    io_conf.mode = GPIO_MODE_OUTPUT;
+	    //io_conf.pin_bit_mask = (1ULL <<(LED_RGB_PIN));  // io_conf.pin_bit_mask = ((1ULL << LED_RED_PIN) | (1ULL << LED_BLUE_PIN) |  BIT(LED_RGB_PIN));
+	    io_conf.pull_down_en = 0;
+	    io_conf.pull_up_en = 0;
+	    gpio_config(&io_conf);
+
+
+	   // LED_RED_OFF();
+	   // LED_BLUE_OFF();
+}
